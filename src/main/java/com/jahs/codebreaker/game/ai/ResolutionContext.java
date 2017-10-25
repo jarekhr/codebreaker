861ab801219edc2458c08f3a9aa6e7bad4d30b89
Jarosław Hryciuk <jarek.hryciuk@gmail.com>
2017-10-25 21:22:05 +0200

    Allocation logic

--- src/main/java/com/jahs/codebreaker/game/ai/ResolutionContext.java
+++ src/main/java/com/jahs/codebreaker/game/ai/ResolutionContext.java
@@ -1,27 +1,38 @@
 package com.jahs.codebreaker.game.ai;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Iterables;
+import com.google.common.collect.*;
 import com.jahs.codebreaker.model.GameConfig;
 import com.jahs.codebreaker.model.PinColor;
 
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+/**
+ * Immutable class which represents a state in result resolution process.
+ *
+ */
 public class ResolutionContext {
 
     private final GameConfig gameConfig;
     private final List<FieldConfig> fields;
 
-    private ResolutionContext(GameConfig gameConfig, List<FieldConfig> fieldConfigs) {
+    private final Set<PinColor> colorsRequiredButNotAllocated;
+
+    private ResolutionContext(GameConfig gameConfig,
+                              List<FieldConfig> fieldConfigs,
+                              Set<PinColor> colorsRequiredButNotAllocated) {
         this.gameConfig = Objects.requireNonNull(gameConfig);
         if (fieldConfigs.size() != gameConfig.getCodeLength()) {
             throw new IllegalArgumentException("Expected " + fieldConfigs.size()
                     + " configs, got: " + fieldConfigs.size());
         }
+
+        // TODO jarek we should try and allocate required colors if possible.
+//        this.fields = tryToAllocateRequired(fieldConfigs)
+
         this.fields = ImmutableList.copyOf(fieldConfigs);
+        this.colorsRequiredButNotAllocated = EnumSet.copyOf(colorsRequiredButNotAllocated);
     }
 
 
@@ -30,7 +41,7 @@ public class ResolutionContext {
         private final Set<PinColor> allowedColors;
 
         public FieldConfig(Set<PinColor> allowedColors) {
-            this.allowedColors = ImmutableSet.copyOf(allowedColors);
+            this.allowedColors = EnumSet.copyOf(allowedColors);
         }
 
         public boolean isSelected() {
@@ -44,6 +55,24 @@ public class ResolutionContext {
         public Set<PinColor> getAllowedColors() {
             return allowedColors;
         }
+
+        public FieldConfig withoutColor(PinColor color) {
+            if (allowedColors.contains(color)) {
+                return new FieldConfig(Sets.difference(allowedColors, EnumSet.of(color)));
+            }
+            return this;
+        }
+
+        public FieldConfig selectColor(PinColor color) {
+            if (allowedColors.contains(color)) {
+                return new FieldConfig(EnumSet.of(color));
+            }
+            throw new IllegalArgumentException("Requested to remove color " + color + ", but it's not here, we only got " + allowedColors);
+        }
+
+        public boolean isFailed() {
+            return allowedColors.isEmpty();
+        }
     }
 
 
@@ -53,11 +82,47 @@ public class ResolutionContext {
         FieldConfig fieldConfig = new FieldConfig(ImmutableSet.copyOf(PinColor.values()));
         ImmutableList.Builder<FieldConfig> configList = ImmutableList.builder();
         gameConfig.getIndexesStream().forEach(i -> configList.add(fieldConfig));
-        return new ResolutionContext(gameConfig, configList.build());
+        return new ResolutionContext(gameConfig, configList.build(), Collections.emptySet());
     }
 
 
     public FieldConfig getFieldConfig(int i) {
         return fields.get(i);
+    }
+
+
+    public ResolutionContext selectColor(int index, PinColor color) {
+        List<FieldConfig> newConfigs = gameConfig.getIndexesStream().mapToObj(i -> {
+            FieldConfig fieldConfig = getFieldConfig(i);
+            if (i == index) {
+                fieldConfig = fieldConfig.selectColor(color);
+            } else {
+                fieldConfig = fieldConfig.withoutColor(color);
+            }
+            return fieldConfig;
+        }).collect(Collectors.toList());
+        return new ResolutionContext(gameConfig, newConfigs, colorsRequiredButNotAllocated);
+    }
+
+    public ResolutionContext excludeColor(PinColor color) {
+        List<FieldConfig> newConfigs = fields.stream()
+                .map(fieldConfig -> fieldConfig.withoutColor(color)).collect(Collectors.toList());
+        return new ResolutionContext(gameConfig, newConfigs, colorsRequiredButNotAllocated);
+    }
+
+
+    public ResolutionContext requireElsewhere(int index, PinColor color) {
+        List<FieldConfig> newConfigs = gameConfig.getIndexesStream().mapToObj(i -> {
+            FieldConfig config = getFieldConfig(i);
+            if (i == index) {
+                config = config.withoutColor(color);
+            }
+            return config;
+        }).collect(Collectors.toList());
+        Set<PinColor> requiredElsewhere = ImmutableSet.<PinColor>builder()
+                .addAll(colorsRequiredButNotAllocated)
+                .add(color)
+                .build();
+        return new ResolutionContext(gameConfig, newConfigs, requiredElsewhere);
     }
 }
